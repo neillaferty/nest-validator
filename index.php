8c34bfd91d6e785d3fa19810c8ec9fecfc17ad52
Neil Laferty <neillaferty@Neils-MacBook-Pro.local>
2021-11-14 08:00:42 -0600

    Adding double quotes to code in results message.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once("includes/class-nest-validator.php");
 
 if ( isset($_POST['code_input'])) {
   $code = $_POST['code_input'];
-  echo '<p>' . $code . ' is ' . (NestValidator::validate_code($code) ? 'valid.' : 'INVALID.') . '</p>';
+  echo '<p>"' . $code . '" is ' . (NestValidator::validate_code($code) ? 'valid.' : 'INVALID.') . '</p>';
 }
 
  ?>
